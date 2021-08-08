f6b255aaae865ab4d7bb23b3f4aa31eccdc07c6f
foamyguy <foamyguy@gmail.com>
2021-08-08 15:24:00 -0500

    adding L pins to metro M0 and M4

--- ports/atmel-samd/boards/metro_m4_express/pins.c
+++ ports/atmel-samd/boards/metro_m4_express/pins.c
@@ -28,6 +28,7 @@ STATIC const mp_rom_map_elem_t board_global_dict_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR_D12), MP_ROM_PTR(&pin_PA17) },
 
     { MP_OBJ_NEW_QSTR(MP_QSTR_LED),MP_ROM_PTR(&pin_PA16) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_L),MP_ROM_PTR(&pin_PA16) },
     { MP_OBJ_NEW_QSTR(MP_QSTR_D13),MP_ROM_PTR(&pin_PA16) },
 
     { MP_OBJ_NEW_QSTR(MP_QSTR_SDA),MP_ROM_PTR(&pin_PB02) },
