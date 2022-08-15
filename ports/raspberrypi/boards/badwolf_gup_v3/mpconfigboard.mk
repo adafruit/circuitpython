5bbb544658f4611255f03e8e7e7f22bf809ce770
Noel Anderson <noel_anderson@outlook.com>
2022-08-15 13:36:29 -0700

    Add custom RP2040 Board

---
+++ ports/raspberrypi/boards/badwolf_gup_v3/mpconfigboard.mk
@@ -0,0 +1,15 @@
+USB_VID = 0x2E8A
+USB_PID = 0x1008
+USB_PRODUCT = "GUPv3"
+USB_MANUFACTURER = "BadWolf"
+
+CHIP_VARIANT = RP2040
+CHIP_FAMILY = rp2
+
+EXTERNAL_FLASH_DEVICES = "W25Q128JVxQ"
+
+CIRCUITPY__EVE = 1
+
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_HID
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
