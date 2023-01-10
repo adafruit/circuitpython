7c4920033ed4c097d036141afc09803c20ed5260
ZodiusInfuser <christopher.parrott2@gmail.com>
2023-01-10 20:48:11 +0000

    Updated Yukon pinout

--- ports/raspberrypi/boards/pimoroni_yukon/pins.c
+++ ports/raspberrypi/boards/pimoroni_yukon/pins.c
@@ -5,67 +5,102 @@ STATIC const qstr board_slot_fields[] = {
     MP_QSTR_FAST1,
     MP_QSTR_FAST2,
     MP_QSTR_FAST3,
-    MP_QSTR_FAST4
+    MP_QSTR_FAST4,
+    MP_QSTR_SLOW1,
+    MP_QSTR_SLOW2,
+    MP_QSTR_SLOW3,
+    MP_QSTR_ADC1_ADDR,
+    MP_QSTR_ADC2_ADDR
 };
 
 STATIC MP_DEFINE_ATTRTUPLE(
     board_slot1_obj,
     board_slot_fields,
-    4,
+    9,
     (mp_obj_t)&pin_GPIO0,
     (mp_obj_t)&pin_GPIO1,
     (mp_obj_t)&pin_GPIO2,
-    (mp_obj_t)&pin_GPIO3
+    (mp_obj_t)&pin_GPIO3,
+    (mp_obj_t)&pin_TCA0_3,
+    (mp_obj_t)&pin_TCA0_4,
+    (mp_obj_t)&pin_TCA0_5,
+    MP_ROM_INT(0),  // 0b0000
+    MP_ROM_INT(3)   // 0b0011
     );
 
 STATIC MP_DEFINE_ATTRTUPLE(
     board_slot2_obj,
     board_slot_fields,
-    4,
+    9,
     (mp_obj_t)&pin_GPIO4,
     (mp_obj_t)&pin_GPIO5,
     (mp_obj_t)&pin_GPIO6,
-    (mp_obj_t)&pin_GPIO7
+    (mp_obj_t)&pin_GPIO7,
+    (mp_obj_t)&pin_TCA0_0,
+    (mp_obj_t)&pin_TCA0_1,
+    (mp_obj_t)&pin_TCA0_2,
+    MP_ROM_INT(1),  // 0b0001
+    MP_ROM_INT(7)   // 0b0110
     );
 
 STATIC MP_DEFINE_ATTRTUPLE(
     board_slot3_obj,
     board_slot_fields,
-    4,
+    9,
     (mp_obj_t)&pin_GPIO8,
     (mp_obj_t)&pin_GPIO9,
     (mp_obj_t)&pin_GPIO10,
-    (mp_obj_t)&pin_GPIO11
+    (mp_obj_t)&pin_GPIO11,
+    (mp_obj_t)&pin_TCA0_8,
+    (mp_obj_t)&pin_TCA0_9,
+    (mp_obj_t)&pin_TCA0_10,
+    MP_ROM_INT(4),  // 0b0100
+    MP_ROM_INT(2)   // 0b0010
     );
 
 STATIC MP_DEFINE_ATTRTUPLE(
     board_slot4_obj,
     board_slot_fields,
-    4,
+    9,
     (mp_obj_t)&pin_GPIO12,
     (mp_obj_t)&pin_GPIO13,
     (mp_obj_t)&pin_GPIO14,
-    (mp_obj_t)&pin_GPIO15
+    (mp_obj_t)&pin_GPIO15,
+    (mp_obj_t)&pin_TCA1_7,
+    (mp_obj_t)&pin_TCA1_6,
+    (mp_obj_t)&pin_TCA1_5,
+    MP_ROM_INT(5),  // 0b0101
+    MP_ROM_INT(7)   // 0b0111
     );
 
 STATIC MP_DEFINE_ATTRTUPLE(
     board_slot5_obj,
     board_slot_fields,
-    4,
+    9,
     (mp_obj_t)&pin_GPIO16,
     (mp_obj_t)&pin_GPIO17,
     (mp_obj_t)&pin_GPIO18,
-    (mp_obj_t)&pin_GPIO19
+    (mp_obj_t)&pin_GPIO19,
+    (mp_obj_t)&pin_TCA1_15,
+    (mp_obj_t)&pin_TCA1_14,
+    (mp_obj_t)&pin_TCA1_13,
+    MP_ROM_INT(8),  // 0b1000
+    MP_ROM_INT(11)  // 0b1011
     );
 
 STATIC MP_DEFINE_ATTRTUPLE(
     board_slot6_obj,
     board_slot_fields,
-    4,
+    9,
     (mp_obj_t)&pin_GPIO20,
     (mp_obj_t)&pin_GPIO21,
     (mp_obj_t)&pin_GPIO22,
-    (mp_obj_t)&pin_GPIO23
+    (mp_obj_t)&pin_GPIO23,
+    (mp_obj_t)&pin_TCA1_10,
+    (mp_obj_t)&pin_TCA1_11,
+    (mp_obj_t)&pin_TCA1_12,
+    MP_ROM_INT(9),  // 0b1001
+    MP_ROM_INT(10)  // 0b1010
     );
 
 STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
@@ -97,6 +132,29 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_INT), MP_ROM_PTR(&pin_GPIO28) },
 
     { MP_ROM_QSTR(MP_QSTR_SHARED_ADC), MP_ROM_PTR(&pin_GPIO29) },
+
+    { MP_ROM_QSTR(MP_QSTR_MAIN_EN), MP_ROM_PTR(&pin_TCA0_6) },
+    { MP_ROM_QSTR(MP_QSTR_USER_SW), MP_ROM_PTR(&pin_TCA0_7) },
+    
+    { MP_ROM_QSTR(MP_QSTR_ADC_ADDR_1), MP_ROM_PTR(&pin_TCA0_12) },
+    { MP_ROM_QSTR(MP_QSTR_ADC_ADDR_2), MP_ROM_PTR(&pin_TCA0_13) },
+    { MP_ROM_QSTR(MP_QSTR_ADC_ADDR_3), MP_ROM_PTR(&pin_TCA0_14) },
+    { MP_ROM_QSTR(MP_QSTR_ADC_MUX_EN_1), MP_ROM_PTR(&pin_TCA0_15) },
+    { MP_ROM_QSTR(MP_QSTR_ADC_MUX_EN_2), MP_ROM_PTR(&pin_TCA0_11) },
+        
+    { MP_ROM_QSTR(MP_QSTR_SW_A), MP_ROM_PTR(&pin_TCA1_1) },
+    { MP_ROM_QSTR(MP_QSTR_SW_B), MP_ROM_PTR(&pin_TCA1_2) },
+    { MP_ROM_QSTR(MP_QSTR_LED_A), MP_ROM_PTR(&pin_TCA1_3) },
+    { MP_ROM_QSTR(MP_QSTR_LED_B), MP_ROM_PTR(&pin_TCA1_4) },
+
+    { MP_ROM_QSTR(MP_QSTR_LCD_BL), MP_ROM_PTR(&pin_TCA1_0) },
+    { MP_ROM_QSTR(MP_QSTR_LCD_DC), MP_ROM_PTR(&pin_TCA1_8) },
+    { MP_ROM_QSTR(MP_QSTR_LCD_CS), MP_ROM_PTR(&pin_TCA1_9) },
+
+    { MP_ROM_QSTR(MP_QSTR_CURRENT_SENSE_ADDR), MP_ROM_INT(12) },    // 0b1100
+    { MP_ROM_QSTR(MP_QSTR_TEMP_SENSE_ADDR), MP_ROM_INT(13) },       // 0b1101
+    { MP_ROM_QSTR(MP_QSTR_VOLTAGE_SENSE_ADDR), MP_ROM_INT(14) },    // 0b1110
+    { MP_ROM_QSTR(MP_QSTR_EX_ADC_ADDR), MP_ROM_INT(15) },           // 0b1111
 
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C), MP_ROM_PTR(&board_i2c_obj) },
