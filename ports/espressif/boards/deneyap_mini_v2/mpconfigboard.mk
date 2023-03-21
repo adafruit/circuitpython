4d7bd90379018e212fc2c73e477eaf8b2b92bff2
Dogus Cendek <55203951+DogushC@users.noreply.github.com>
2023-03-21 11:29:41 +0300

    Fix End of Files
    Fix End of Files

--- ports/espressif/boards/deneyap_mini_v2/mpconfigboard.mk
+++ ports/espressif/boards/deneyap_mini_v2/mpconfigboard.mk
@@ -9,6 +9,3 @@ IDF_TARGET = esp32s2
 CIRCUITPY_ESP_FLASH_MODE = dio
 CIRCUITPY_ESP_FLASH_FREQ = 80m
 CIRCUITPY_ESP_FLASH_SIZE = 4MB
-
-# Include these Python libraries in firmware.
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
