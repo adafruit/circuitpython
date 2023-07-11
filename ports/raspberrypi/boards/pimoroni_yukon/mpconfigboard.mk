8b186c6983abc862a42790fe2fc6ef8c0e2aa82b
ZodiusInfuser <christopher.parrott2@gmail.com>
2023-07-11 16:44:01 +0100

    Added frozen modules for motors and LEDs

--- ports/raspberrypi/boards/pimoroni_yukon/mpconfigboard.mk
+++ ports/raspberrypi/boards/pimoroni_yukon/mpconfigboard.mk
@@ -11,3 +11,8 @@ EXTERNAL_FLASH_DEVICES = "W25Q128JVxQ"
 CIRCUITPY__EVE = 1
 
 CIRCUITPY_TCA9555R = 1
+
+# Include these Python libraries in firmware.
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Motor
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_DotStar
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
