827862e158b9bf75dce6b6baf715bd546a4fbd15
ZodiusInfuser <christopher.parrott2@gmail.com>
2023-09-14 17:23:01 +0100

    Updated pins for new hardware, and addressed a issued raised in PR

--- ports/raspberrypi/bindings/tca9555r/__init__.c
+++ ports/raspberrypi/bindings/tca9555r/__init__.c
@@ -3,8 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2022 Jeff Epler for Adafruit Industries
- * Copyright (c) 2016 Scott Shawcroft
+ * Copyright (c) 2023 Christopher Parrott for Pimoroni
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
