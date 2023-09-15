9353cfb4a06ded0f0875e81c0f10322a9a9165d2
ZodiusInfuser <christopher.parrott2@gmail.com>
2023-09-15 20:01:32 +0100

    Update company name in copyright

--- shared-bindings/tca9555/__init__.c
+++ shared-bindings/tca9555/__init__.c
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2023 Christopher Parrott for Pimoroni
+ * Copyright (c) 2023 Christopher Parrott for Pimoroni Ltd
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
