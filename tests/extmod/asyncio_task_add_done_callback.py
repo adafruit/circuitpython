e4439c5af39b73ecdf000d6ab038966894aff9d6
James Ward <james@notjam.es>
2023-11-10 18:53:12 -0500

    test: add tests for new task result and exception behavior

---
+++ tests/extmod/asyncio_task_add_done_callback.py
@@ -0,0 +1,50 @@
+# Test the Task.done() method
+
+try:
+    import asyncio
+except ImportError:
+    print("SKIP")
+    raise SystemExit
+
+
+async def task(t, exc=None):
+    if t >= 0:
+        await asyncio.sleep(t)
+    if exc:
+        raise exc
+
+
+async def main():
+    # Tasks that aren't done only execute done callback after finishing
+    print("=" * 10)
+    t = asyncio.create_task(task(-1))
+    t.add_done_callback(lambda: print("done"))
+    print("Waiting for task to complete")
+    await asyncio.sleep(0)
+    print("Task has completed")
+
+    # Task that are done run the callback immediately
+    print("=" * 10)
+    t = asyncio.create_task(task(-1))
+    await asyncio.sleep(0)
+    print("Task has completed")
+    t.add_done_callback(lambda: print("done"))
+    print("Callback Added")
+
+    # Task that starts, runs and finishes without an exception should return None
+    print("=" * 10)
+    t = asyncio.create_task(task(0.01))
+    t.add_done_callback(lambda: print("done"))
+    try:
+        t.add_done_callback(lambda: print("done"))
+    except RuntimeError as e:
+        print("Second call to add_done_callback emits error:", repr(e))
+
+    # Task that raises immediately should still run done callback
+    print("=" * 10)
+    t = asyncio.create_task(task(-1, ValueError))
+    t.add_done_callback(lambda: print("done"))
+    await asyncio.sleep(0)
+
+
+asyncio.run(main())
