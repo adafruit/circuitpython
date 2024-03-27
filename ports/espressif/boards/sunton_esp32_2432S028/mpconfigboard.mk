a0cd11c5108591fd197a6f451755ccac16d6423e
RetiredWizard <github@retiredwizard.com>
2024-03-27 02:38:53 -0400

    Board: sunton_esp32_2432S028 Add Frozen Libraries

--- ports/espressif/boards/sunton_esp32_2432S028/mpconfigboard.mk
+++ ports/espressif/boards/sunton_esp32_2432S028/mpconfigboard.mk
@@ -8,3 +8,9 @@ CIRCUITPY_ESP_FLASH_FREQ = 80m
 CIRCUITPY_ESP_FLASH_SIZE = 4MB
 
 CIRCUITPY_ESPCAMERA = 0
+
+# Include these Python libraries in firmware.
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_ConnectionManager
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Requests
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Display_Text
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_ImageLoad
