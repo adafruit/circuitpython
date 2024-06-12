8d32a53194c0ccabd0678783cab3da73a291af19
Bill Sideris <bill88t@feline.gr>
2024-06-12 20:22:27 +0300

    ... Types.

--- ports/silabs/common-hal/analogio/AnalogIn.c
+++ ports/silabs/common-hal/analogio/AnalogIn.c
@@ -185,7 +185,7 @@ void IADC_IRQHandler(void) {
 //  adc value 0 - 65535
 uint16_t common_hal_analogio_analogin_get_value(analogio_analogin_obj_t *self) {
     uint32_t avg_value = 0;
-    for (int i = 0; i < self->sample_size; i++) {
+    for (uint16_t i = 0; i < self->sample_size; i++) {
         // Start scan
         IADC_command(IADC0, iadcCmdStartScan);
 
