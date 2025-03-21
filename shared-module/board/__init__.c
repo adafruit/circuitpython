424ea8861e1a124c42e475c4545492c264c6e131
RetiredWizard <github@retiredwizard.com>
2025-03-21 17:19:03 -0400

    don't define max_num_displays when not needed

--- shared-module/board/__init__.c
+++ shared-module/board/__init__.c
@@ -176,7 +176,7 @@ mp_obj_t common_hal_board_create_uart(const mp_int_t instance) {
 #endif
 
 void reset_board_buses(void) {
-    #if CIRCUITPY_BOARD_I2C || (CIRCUITPY_BOARD_SPI && (CIRCUITPY_FOURWIRE || CIRCUITPY_SHARPDISPLAY || CIRCUITPY_AURORA_EPAPER))
+    #if (CIRCUITPY_BOARD_I2C && CIRCUITPY_I2CDISPLAYBUS) || (CIRCUITPY_BOARD_SPI && (CIRCUITPY_FOURWIRE || CIRCUITPY_SHARPDISPLAY || CIRCUITPY_AURORA_EPAPER))
     mp_int_t max_num_displays = CIRCUITPY_DISPLAY_LIMIT;
     #if CIRCUITPY_OS_GETENV
     #define DYN_DISPLAY_BUSES(indx) (indx < CIRCUITPY_DISPLAY_LIMIT ? display_buses[indx] : display_buses_dyn[indx - CIRCUITPY_DISPLAY_LIMIT])
