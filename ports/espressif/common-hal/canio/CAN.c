db54de43a0f1377d587f170fdb5903c67fd823cd
EricBaril72 <eric@baril.com>
2026-05-06 20:00:23 -0400

    applied pull request comments

--- ports/espressif/common-hal/canio/CAN.c
+++ ports/espressif/common-hal/canio/CAN.c
@@ -14,17 +14,19 @@
 #include "shared-bindings/util.h"
 #include "supervisor/port.h"
 
-#include "hal/twai_types.h"
+#include "soc/twai_periph.h"
 
 static byte reserved_can_instance; /* Bit assigned instances    0x01 (CAN0)  or 0x02 (CAN1)    0x00-NoneAssigned     0x03-Both Assigned*/
 
-#define TWAI0              REG_TWAI_BASE(0) /**< \brief (CAN0) APB Base Address */
-#define TWAI1              REG_TWAI_BASE(1) /**< \brief (CAN1) APB Base Address */
-#define TWAI_INST_NUM      2                          /**< \brief (CAN) Number of instances */
+#if SOC_TWAI_CONTROLLER_NUM == 1
+static twai_handle_t twai_bus_0;
+static twai_handle_t *const twai_insts[SOC_TWAI_CONTROLLER_NUM] = { &twai_bus_0 };
 
+#elif SOC_TWAI_CONTROLLER_NUM == 2
 static twai_handle_t twai_bus_0;
 static twai_handle_t twai_bus_1;
-static twai_handle_t *const twai_insts[TWAI_INST_NUM] = { &twai_bus_0, &twai_bus_1 };
+static twai_handle_t *const twai_insts[SOC_TWAI_CONTROLLER_NUM] = { &twai_bus_0, &twai_bus_1 };
+#endif
 
 static twai_timing_config_t get_t_config(int baudrate) {
     switch (baudrate) {
@@ -108,32 +110,30 @@ static twai_timing_config_t get_t_config(int baudrate) {
 void common_hal_canio_can_construct(canio_can_obj_t *self, const mcu_pin_obj_t *tx, const mcu_pin_obj_t *rx, int baudrate, bool loopback, bool silent) {
 #define DIV_ROUND(a, b) (((a) + (b) / 2) / (b))
 #define DIV_ROUND_UP(a, b) (((a) + (b) - 1) / (b))
-    if (reserved_can_instance==3) {
+    if (reserved_can_instance == (1 << SOC_TWAI_CONTROLLER_NUM) - 1) {
+        /* 3 controller all taken => reserved_can_instance = 7
+        *  1 << 3 is 8
+        */
         mp_raise_ValueError(MP_ERROR_TEXT("All CAN peripherals are in use"));
+    } else if (reserved_can_instance > SOC_TWAI_CONTROLLER_NUM) {
+        mp_raise_ValueError(MP_ERROR_TEXT("This cpu only supports 1 CAN peropheral"));
     }
+
 
     if (loopback && silent) {
         mp_raise_ValueError(MP_ERROR_TEXT("loopback + silent mode not supported by peripheral"));
     }
 
-    byte this_can_instance=0;
-    if (reserved_can_instance==0x00){   /* None assigned */
-        reserved_can_instance=0x01;     /* First instance is assigned*/ 
-        this_can_instance=0;            /* Idx of selected instance */
+    byte this_can_instance = 0;
+    /* reserved_can_instance lowest significant bit at 0 is available */
+    while (!(reserved_can_instance & (1 << this_can_instance))) {
+        this_can_instance++;
     }
-    else if (reserved_can_instance==0x01) /* First instance allready assigned */
-    {
-        reserved_can_instance=0x03;     /* Second instance also assigned */
-        this_can_instance=1;            /* Idx of selected instance */
+    reserved_can_instance |= (1 << this_can_instance);
 
-    }else if (reserved_can_instance==0x02)  /* Second instance is assigned */
-    {
-        reserved_can_instance=0x03;     /* first instance also assigned*/ 
-        this_can_instance=0;            /* Idx of selected instance */
-    }
-    
+
     twai_timing_config_t t_config = get_t_config(baudrate);
-    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT_V2(this_can_instance,-1, -1, TWAI_MODE_NORMAL);
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT_V2(this_can_instance, -1, -1, TWAI_MODE_NORMAL);
     g_config.tx_io = tx->number;
     g_config.rx_io = rx->number;
     g_config.controller_id = this_can_instance;   /* To ease later de-initing of proper instance */
@@ -146,7 +146,7 @@ void common_hal_canio_can_construct(canio_can_obj_t *self, const mcu_pin_obj_t *
     }
 
     twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
-    esp_err_t result = twai_driver_install_v2(&g_config, &t_config, &f_config , twai_insts[this_can_instance]);
+    esp_err_t result = twai_driver_install_v2(&g_config, &t_config, &f_config, twai_insts[this_can_instance]);
     if (result == ESP_ERR_NO_MEM) {
         mp_raise_msg(&mp_type_MemoryError, MP_ERROR_TEXT("ESP-IDF memory allocation failed"));
     } else if (result == ESP_ERR_INVALID_ARG) {
@@ -154,7 +154,7 @@ void common_hal_canio_can_construct(canio_can_obj_t *self, const mcu_pin_obj_t *
     } else if (result != ESP_OK) {
         mp_raise_OSError_msg_varg(MP_ERROR_TEXT("twai_driver_install returned esp-idf error #%d"), (int)result);
     }
-    
+
     result = twai_start_v2(*twai_insts[this_can_instance]);
     if (result != ESP_OK) {
         mp_raise_OSError_msg_varg(MP_ERROR_TEXT("twai_start returned esp-idf error #%d"), (int)result);
@@ -172,7 +172,7 @@ void common_hal_canio_can_construct(canio_can_obj_t *self, const mcu_pin_obj_t *
     self->twai_driver_handle = twai_insts[this_can_instance];
     self->instance = this_can_instance;
 
-    
+
 }
 
 
@@ -188,21 +188,21 @@ int common_hal_canio_can_baudrate_get(canio_can_obj_t *self) {
 
 int common_hal_canio_can_transmit_error_count_get(canio_can_obj_t *self) {
     twai_status_info_t info;
-    twai_get_status_info_v2( *self->twai_driver_handle,&info);
+    twai_get_status_info_v2(*self->twai_driver_handle, &info);
     return info.tx_error_counter;
 }
 
 int common_hal_canio_can_receive_error_count_get(canio_can_obj_t *self) {
     twai_status_info_t info;
-    twai_get_status_info_v2( *self->twai_driver_handle,&info);
+    twai_get_status_info_v2(*self->twai_driver_handle, &info);
     return info.rx_error_counter;
 }
 
 
 canio_bus_state_t common_hal_canio_can_state_get(canio_can_obj_t *self) {
     twai_status_info_t info;
-    
-    twai_get_status_info_v2( *self->twai_driver_handle,&info);
+
+    twai_get_status_info_v2(*self->twai_driver_handle, &info);
     if (info.state == TWAI_STATE_BUS_OFF || info.state == TWAI_STATE_RECOVERING) {
         return BUS_STATE_OFF;
     }
@@ -226,7 +226,7 @@ static void can_restart(canio_can_obj_t *self) {
     // wait 100ms (hard coded for now) for bus to recover
     uint64_t deadline = port_get_raw_ticks(NULL) + 100;
     do {
-        twai_get_status_info_v2(*self->twai_driver_handle,&info);
+        twai_get_status_info_v2(*self->twai_driver_handle, &info);
     } while (port_get_raw_ticks(NULL) < deadline && (info.state == TWAI_STATE_BUS_OFF || info.state == TWAI_STATE_RECOVERING));
 }
 
@@ -269,7 +269,7 @@ void common_hal_canio_can_send(canio_can_obj_t *self, mp_obj_t message_in) {
         memcpy(message_out.data, message->data, message->size);
     }
     // Allow transmission to occur in background
-    twai_transmit_v2(*self->twai_driver_handle,&message_out, 0);
+    twai_transmit_v2(*self->twai_driver_handle, &message_out, 0);
 }
 
 bool common_hal_canio_can_silent_get(canio_can_obj_t *self) {
@@ -293,12 +293,8 @@ void common_hal_canio_can_deinit(canio_can_obj_t *self) {
         reset_pin_number(self->tx_pin->number);
         reset_pin_number(self->rx_pin->number);
 
-        if (self->instance==0){
-            reserved_can_instance&=0x02; /* free first instance - clear 1st bit */
-        }else if (self->instance==1){
-            reserved_can_instance&=0x01; /* free second instance - clear 2nd bit */
-        }
-        
+        reserved_can_instance &= ~(1 << self->instance);
+
     }
     self->tx_pin = NULL;
     self->rx_pin = NULL;
@@ -309,5 +305,5 @@ void common_hal_canio_reset(void) {
     (void)twai_driver_uninstall_v2(*twai_insts[0]);
     (void)twai_stop_v2(*twai_insts[1]);
     (void)twai_driver_uninstall_v2(*twai_insts[1]);
-    reserved_can_instance=0; /* unassign both bits */
+    reserved_can_instance = 0; /* unassign both bits */
 }
